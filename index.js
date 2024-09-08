cb98c894b9d550c39171d8c32125305cfe6b0e5a
AbdulBaseer1939 <ninja19391234@gmail.com>
2024-09-08 23:40:21 +0500

    quiz completed

--- index.js
+++ index.js
@@ -1,56 +1,95 @@
-// Adding Questions
-
-var questions = [
+var data = [
   {
     question: "What is the capital of France?",
-    choices: ["Berlin", "Madrid", "Paris", "Lisbon"],
-    correctAnswer: 2,
+    a: "London",
+    b: "Paris",
+    c: "Istumbol",
+    d: "Islamabad",
+    correct: "b",
   },
   {
     question: "Which planet is known as the Red Planet?",
-    choices: ["Earth", "Mars", "Jupiter", "Saturn"],
-    correctAnswer: 2,
+    a: "Mars",
+    b: "Earth",
+    c: "Jupiter",
+    d: "Saturn",
+    correct: "a",
   },
   {
-   question: "What is the capital of Italy?",
-   choices: [Rome, Milan, Naples, Venice],
-   correctAnswer: 1,
+    question: "What is the capital of Italy?",
+    a: "Milan",
+    b: "Rome",
+    c: "Naples",
+    d: "Venice",
+    correct: "b",
   },
   {
-   question: "What is the capital of Spain?",
-   choices: [Barcelona, Madrid, Valencia, Seville],
-   correctAnswer: 2,
-  },
-  {question: "What is the capital of Germany?",
-   choices: [Munich, Berlin, Frankfurt, Hamburg],
-   correctAnswer: 2,
-  },
-  {
-   question: "What is the capital of Russia?",
-   choices: [Moscow, Saint-Petersburg, Kazan, Novosibirsk],
-   correctAnswer: 1,
-  },
-  {
-   question: "What is the capital of the United Kingdom?",
-   choices: [London, Edinburgh, Birmingham, Manchester],
-   correctAnswer: 1,
-  },
-  {
-   questionuestion: "What is the capital of China?",
-   choices: [Beijing, Shanghai, Guangzhou, Shenzhen],
-   correctAnswer: 1,
-  },
-  {
-   questionuestion: "What is the capital of Japan?",
-   choices: [Tokyo, Osaka, Kyoto, Nagoya],
-   correctAnswer: 1,
-  },
-  {
-    question: "What is the capital of India?",
-   choices: [Delhi, Mumbai, Kolkata, Chennai],
-   correctAnswer: 1,
+    question: "What is the capital of Spain?",
+    a: "Barcelona",
+    b: "Valencia",
+    c: "Madrid",
+    d: "Seville",
+    correct: "c",
   },
 ];
 
-let currentQuestionIndex = 0;
+var quiz = document.getElementById("quiz");
+var answerEls = document.querySelectorAll(".answer");
+var questionE1 = document.getElementById("question");
+var optionA = document.getElementById("optionA");
+var optionB = document.getElementById("optionB");
+var optionC = document.getElementById("optionC");
+var optionD = document.getElementById("optionD");
+var submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
 let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+
+  var currentQuizData = data[currentQuiz];
+
+  questionE1.innerText = currentQuizData.question;
+  optionA.innerText = currentQuizData.a;
+  optionB.innerText = currentQuizData.b;
+  optionC.innerText = currentQuizData.c;
+  optionD.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+
+function getSelected() {
+  let answer;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+submitBtn.addEventListener("click", () => {
+  var answer = getSelected();
+
+  if (answer) {
+    if (answer.toLowerCase() === data[currentQuiz].correct.toLowerCase()) {
+      score++;
+    }
+
+    currentQuiz++;
+
+    if (currentQuiz < data.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h2> You Answered ${score}/${data.length} Questions Correctly</h2>
+      <button onclick="location.reload()" class="btn">Do it Again</button>`;
+    }
+  }
+});
